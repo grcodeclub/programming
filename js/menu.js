dbf6f64183f1260d1038cbe38f8fcb2645d93b1c
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-13 15:22:24 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    
+    var  menuLink = '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.gr/programming/">Προγραμματισμός</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/programming/post">Περισσότερα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.gr/programming/orologia">Oρολογία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/c">C</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/cpp">C++</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/java">Java</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/python">Python</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.gr/nodejs/">Node.js</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.gr/sql">SQL</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/docker">Docker</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.gr/programming/markdown">Markdown</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    
+  });
+  
---
+++ vscode_extension.md
@@ -0,0 +1,26 @@
+# VsCode Extension 
+## Python 
+__Name:__ Python<br>
+__Id:__ ms-python.python
+
+## Preview
+__Name:__ Live Preview
+__Id:__ ms-vscode.live-server
+
+## C/C++
+__Name:__ C/C++<br>
+__Id:__ ms-vscode.cpptools
+
+## Html - CSS
+__Name:__ HTML Preview<br>
+__Id:__ george-alisson.html-preview-vscode
+
+__Name:__ HTML CSS Support<br>
+__Id:__ ecmel.vscode-html-css
+
+## Markdown
+__Name:__ Markdown Preview Enhanced <br>
+__Id:__ shd101wyy.markdown-preview-enhanced
+
+__Name:__ Markdown Export DF,PNG,HTML,JPEG<br>
+__Id:__ yzane.markdown-pdf
