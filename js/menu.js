01c01490287a98bf65a643e475bd1f56923492b2
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-18 19:22:10 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -24,9 +24,6 @@ $(document).ready(function() {
     menuLink = '<li><a href="https://grcodeclub.gr/python">Python</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = '<li><a href="https://grcodeclub.gr/nodejs/">Node.js</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
     menuLink = '<li><a href="https://grcodeclub.gr/sql">SQL</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
